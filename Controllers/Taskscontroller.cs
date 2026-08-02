e4e5085902d99211251ed76fe2cf9b10534fdec3
Navya Arya <navyaarya2706@gmail.com>
2026-08-02 18:56:51 +0530

    Add project files.

---
+++ Controllers/Taskscontroller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Taskflow.Api.Models;
+
+namespace Taskflow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Taskscontroller : ControllerBase
+    {
+        private static readonly List<TaskItem> _tasks = new()
+        {
+            new TaskItem { Id = 1, Title = "Task 1", Description = "Description for Task 1", IsCompleted = false },
+            new TaskItem { Id = 2, Title = "Task 2", Description = "Description for Task 2", IsCompleted = true },
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetTasks()
+        {
+            return Ok(_tasks);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
