76b81958d7ca919c0d6dbea2fa3c3c5c8c4edb7a
Navya Arya <navyaarya2706@gmail.com>
2026-08-05 00:58:51 +0530

    TaskflowDBContext

---
+++ Taskflow.Api/Data/TaskFlowDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taskflow.Api.Models;
+
+
+namespace Taskflow.Api.Data
+{
+    public class TaskFlowDbContext :DbContext
+    {
+        public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> options) :base(options)
+        {
+
+        }
+        public DbSet<TaskItem> Tasks { get; set; } = null!;
+    }
+}
