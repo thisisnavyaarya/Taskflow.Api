7dde8da904cf470bafcf88bf59af4af1cd5b170e
Navya Arya <navyaarya2706@gmail.com>
2026-08-05 00:59:13 +0530

    Program.cs

--- Taskflow.Api/Program.cs
+++ Taskflow.Api/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Taskflow.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<TaskFlowDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
